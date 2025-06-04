73a632db856d564bd7e99b33a460c5293a1e5f81
camryntobias <158119551+camryntobias@users.noreply.github.com>
2025-06-04 13:44:30 -0700

    Add files via upload

--- Final Project/config.js
+++ Final Project/config.js
@@ -15,9 +15,9 @@ var config = {
     theme: 'dark',
     use3dTerrain: true, //set true for enabling 3D maps.
     auto: false,
-    title: 'Your Title Goes Here',
-    subtitle: 'The Storytelling Template helps you create an awesome animated map story with ease.',
-    byline: 'By a I.M. Amapper',
+    title: 'Mapping the John Muir Trail',
+    subtitle: 'The ins and outs of thru-hiking the spectacular Eastern Sierra Mountains',
+    byline: 'By Camryn Tobias',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         /*
@@ -82,7 +82,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Mile 0',
-            image: './assets/buenos-aires.jpg',
+            image: './JMTPhotos/IMG_1412.jpg',
             description: 'Arrive at the Backpackers Campground in Yosemite National Park.',
             location: {
                 center: [-119.56681985671841, 37.74442799139182],
@@ -104,8 +104,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Mile 6.5',
-            image: '',
-            description: '',
+            image: './JMTPhotos/IMG_1431.jpg',
+            description: 'Happy Isles to Sunrise High Sierra Camp. Get on the trail early to avoid the crowds in the park. The climb out of the valley is grueling but sets a nice tone for the journey that awaits. In addition to the JMT permit, it is possible to acquire a permit for Half Dome. While it is not on the trail, summiting Half Dome is extremely worth while.',
             location: {
                 center: [-119.43238018295273, 37.7940758334673],
                 zoom: 14,
@@ -126,8 +126,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Mile 22',
-            image: './assets/donohue.jpg',
-            description: 'One of the iconic high passes on the JMT.',
+            image: './JMTPhotos/IMG_1497.jpg',
+            description: 'Sunrise High Sierra Camp to Lyell Canyon. Have lunch at the Tuolumne Meadows Grill.',
             location: {
                 center: [-119.28996236120614, 37.84840782077943],
                 zoom: 13,
@@ -145,8 +145,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Mile 36',
-            image: './assets/donohue.jpg',
-            description: 'One of the iconic high passes on the JMT.',
+            image: './JMTPhotos/IMG_1553.jpg',
+            description: 'Lyell Canyon over Donohue Pass. Donohue Pass is thr first major pass at 10,000 ft.',
             location: {
                 center: [-119.21172115361357, 37.7434239389048],
                 zoom: 13,
@@ -164,8 +164,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Mile 59',
-            image: './assets/donohue.jpg',
-            description: 'One of the iconic high passes on the JMT.',
+            image: './JMTPhotos/IMG_1585.jpg',
+            description: 'Reds Meadow Resort. Resupply food, wash clothes, and take a shower.',
             location: {
                 center: [-119.07497021228784, 37.61942660081634],
                 zoom: 13,
@@ -183,8 +183,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Mile 74',
-            image: './assets/donohue.jpg',
-            description: 'One of the iconic high passes on the JMT.',
+            image: './JMTPhotos/IMG_1608.jpg',
+            description: 'Lake Virginia.',
             location: {
                 center: [-118.93520169854978, 37.515789933863346],
                 zoom: 13,
@@ -192,7 +192,7 @@ var config = {
                 bearing: 190
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: false,
+            rotateAnimation: true,
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
@@ -202,8 +202,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Mile 109',
-            image: './assets/donohue.jpg',
-            description: 'One of the iconic high passes on the JMT.',
+            image: './JMTPhotos/IMG_1632.jpg',
+            description: 'Muir Trail Ranch, resupply food and properly dispose of trash.',
             location: {
                 center: [-118.8813932802017, 37.2379557629022],
                 zoom: 13,
@@ -221,8 +221,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Mile 127',
-            image: './assets/donohue.jpg',
-            description: 'One of the iconic high passes on the JMT.',
+            image: './JMTPhotos/IMG_0197.jpeg',
+            description: 'McClure Meadow. Tough climb out of Muir Trail Ranch, especially if your backpack is freshly heavy and full from resupply.',
             location: {
                 center: [-118.74170341975324, 37.18668242879487],
                 zoom: 13,
@@ -240,8 +240,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Mile 141',
-            image: './assets/donohue.jpg',
-            description: 'One of the iconic high passes on the JMT.',
+            image: './JMTPhotos/IMG_0202.jpeg',
+            description: 'Muir Pass at 11,955 ft., on the top of the pass is a John Muir Memorial Shelter, in the case of an unexpected weather event.',
             location: {
                 center: [-118.67048982743975, 37.111972141006675],
                 zoom: 13,
@@ -249,7 +249,7 @@ var config = {
                 bearing: 196
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: false,
+            rotateAnimation: true,
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
@@ -259,8 +259,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Mile 157',
-            image: './assets/donohue.jpg',
-            description: 'One of the iconic high passes on the JMT.',
+            image: './JMTPhotos/IMG_1786.jpg',
+            description: 'Over Mather Pass at 12,0000 ft. arguably that hardest pass on the trail. Camp at Lake Marjorie',
             location: {
                 center: [-118.42915868612923, 36.94574645939778],
                 zoom: 13,
@@ -268,7 +268,7 @@ var config = {
                 bearing: 198
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: false,
+            rotateAnimation: true,
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
@@ -278,8 +278,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Mile 172',
-            image: './assets/donohue.jpg',
-            description: 'One of the iconic high passes on the JMT.',
+            image: './JMTPhotos/IMG_0201.jpg',
+            description: 'Over Pinchot pass at 12,100 ft. and camp at Rae Lakes.',
             location: {
                 center: [-118.40052973246995, 36.8102792205875],
                 zoom: 13,
@@ -287,7 +287,7 @@ var config = {
                 bearing: 200
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: false,
+            rotateAnimation: true,
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
@@ -297,8 +297,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Mile 192',
-            image: './assets/donohue.jpg',
-            description: 'One of the iconic high passes on the JMT.',
+            image: './JMTPhotos/IMG_1827.jpg',
+            description: 'Over Foresters pass at 13,200 ft., the highest mountain pass on the John Muir Trail. Camp at Tyndall Creek.',
             location: {
                 center: [-118.38502982950367, 36.63504350708706],
                 zoom: 13,
@@ -316,8 +316,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Mile 205',
-            image: './assets/donohue.jpg',
-            description: 'One of the iconic high passes on the JMT.',
+            image: './JMTPhotos/IMG_0204.jpeg',
+            description: 'Guitar Lake. Last Camp sight on the John Muir Trail.',
             location: {
                 center: [-118.31501696659642, 36.57112958200143],
                 zoom: 13,
@@ -335,8 +335,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Mile 211',
-            image: './assets/donohue.jpg',
-            description: 'One of the iconic high passes on the JMT.',
+            image: './JMTPhotos/IMG_1907.jpg',
+            description: 'Summit of Mt. Whitney 14,505 ft., highest mountain in the contiguous United States and the official end point of the JMT.',
             location: {
                 center: [-118.29213666941982, 36.57860298708489],
                 zoom: 13,
@@ -344,7 +344,7 @@ var config = {
                 bearing: 206
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: false,
+            rotateAnimation: true,
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
@@ -354,8 +354,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Mile 222',
-            image: './assets/donohue.jpg',
-            description: 'One of the iconic high passes on the JMT.',
+            image: './JMTPhotos/IMG_1942.jpg',
+            description: 'Whitney Portal. Finally off the trail.',
             location: {
                 center: [-118.24013307501761, 36.586772545750364],
                 zoom: 13,
